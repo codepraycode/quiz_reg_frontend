ae2354cfa48329f8b44135bf6b4d16b75ef2088e
Precious Olusola <preciousolusola16@gmail.com>
2022-04-27 00:05:47 +0100

    working on the registration form

--- src/components/forms/registration_form.js
+++ src/components/forms/registration_form.js
@@ -1,11 +1,54 @@
 import React from 'react';
 
 const RegistrationForm = () => {
+    /* 
+        step 1:
+            - passport
+            - first name
+            - other name
+            - last name
+            - gender
+            - date_of_birth, age
+        step 2:
+            - region
+            - province
+            - zone
+            - area
+            - parish
+        step 3:
+            - participant category
+            - quiz_category
+            - birth_certificate
+            - letter of recommendation
+            - regional_coordinator
+            - provincial_coordinator
+        step 4:
+            * summary and submit
+
+    */
+   const renderProgress = ()=>{
+       return (
+           <ul className="progressbar">
+                <li className="active" id="pr">
+                    <strong>Participant Information</strong></li>
+                <li  id="tr"><strong>Parish Tree</strong></li>
+                <li id="qz"><strong>Quiz Setup</strong></li>
+                <li id="sm"><strong>Summary</strong></li>
+            </ul>
+       )
+   }
     return (
-        <form className="registration_form" onSubmit={(e)=>{e.preventDefault();}}>
-                <h2 className="title">Registration</h2>
+        <form className="registration_form card px-0 pt-4 pb-0 mt-3 mb-3" onSubmit={(e)=>{e.preventDefault();}}>
+            
+                <h2 className="title">
+                    Registration
+                </h2>
                 
-                <div className="input_field">
+                <>
+                   {renderProgress()}
+                </>
+
+                {/* <div className="input_field">
                     <i className="fas fa-user"></i>
                     <input type="text" placeholder="Username" />
                     
@@ -18,7 +61,7 @@ const RegistrationForm = () => {
                 
                 <button type="submit" value="Login" className="btn solid">
                     Submit
-                    </button>
+                    </button> */}
                 
             </form>
         
--- src/views/registration.js
+++ src/views/registration.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import RegistrationForm from '../components/forms/registration_form';
 
 
 const Registration = (props) => {
+
+    
+    useEffect(() => {
+    document.title = "Participant Registration | Quiz Registration"
+
+    return () => {
+      //your cleanup code codes here
+      document.title = "Quiz Registration"
+    };
+
+  }, [])
+
     return (
         <>
             <RegistrationForm {...props}/>
