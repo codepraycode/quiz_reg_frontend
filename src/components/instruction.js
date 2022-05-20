62aa30e3adde4b6ccf77c901e31736b76a63c33b
Precious Olusola <preciousolusola16@gmail.com>
2022-05-20 02:45:42 +0100

    updated portal instructions

--- src/components/instruction.js
+++ src/components/instruction.js
@@ -11,6 +11,14 @@ const Instruction = () => {
             <div className="card_body">
                 <p className="text-muted">
                     Hello! This is a new Junior Church Competition Registration System for you. It is user friendly. The platform ensures a seamless registration. You can register for the quiz, song ministration, debate, essay writing, recitation, spelling Bee, other activities( drama, song and others), Zonal Presentation and upload participant's documents, register your courses, and manage your region results with more flexibilities than before. Provide your region email and password to access your account. Your Password would be communicated to you via your region dedicated email.
+                    <br/>
+
+                    <strong><b>Note:</b></strong> Please each region is to come with five(5) good laptops for the quiz competition.
+
+                    <br/>
+                    For quiz practise, go to <a target="_blank" rel="noreferrer" href="https://rccgjuniorchurch.org.ng">https://rccgjuniorchurch.org.ng</a>, then
+                    navigate to <b>National Quiz Competition</b>(or <a target="_blank" rel="noreferrer" href="https://quizpractise.rccgjuniorchurch.org.ng">https://quizpractise.rccgjuniorchurch.org.ng</a>)
+
                 </p>
 
 
